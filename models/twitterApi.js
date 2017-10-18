03ebda8522439fe9d507b47ca4cb51113b3472d9
NNcrawler <rasyid.xyz@gmail.com>
2017-10-18 13:20:05 +0700

    finish model get timeline, search, post

--- models/twitterApi.js
+++ models/twitterApi.js
@@ -9,15 +9,55 @@ var oauth = new OAuth.OAuth(
     null,
     'HMAC-SHA1'
   );
-  oauth.get(
-    'https://api.twitter.com/1.1/trends/place.json?id=23424977',
-    '920498793220591616-t8vCp2kqKoTkfnibY3gqIFGbLALg7B9', //test user token
-    'VxhzJe2LfNx87uLX1HCGueuWEiXPzpDuEO7tJV3g67zkC', //test user secret
-    function (e, data, res){
-      if (e) console.error(e);
-      console.log(require('util').inspect(data));
-    });
+
+function useApiPromise(url){
+  return new Promise((resolve, reject)=>{
+    oauth.get(
+      url,
+      '920498793220591616-t8vCp2kqKoTkfnibY3gqIFGbLALg7B9', //test user token
+      'VxhzJe2LfNx87uLX1HCGueuWEiXPzpDuEO7tJV3g67zkC', //test user secret
+      function (e, data, res){
+        if (e) reject(e);
+        else resolve(data)
+      });
+  })
+}
+
+function postApiPromise(url, obj){
+  return new Promise((resolve, reject)=>{
+    oauth.post(
+      url,
+      '920498793220591616-t8vCp2kqKoTkfnibY3gqIFGbLALg7B9', //test user token
+      'VxhzJe2LfNx87uLX1HCGueuWEiXPzpDuEO7tJV3g67zkC', //test user secret
+      obj,
+      function (e, data, res){
+        if (e) reject(e);
+        else resolve(data)
+      });
+  })
+}
 
 class TwitterAPI{
+  static getAllTimeline(){
+    return useApiPromise('https://api.twitter.com/1.1/statuses/home_timeline.json')
+  }
 
+  static getSearchResult(query){
+    return useApiPromise(`https://api.twitter.com/1.1/search/tweets.json?q=${query}`)
+  }
+
+  static postTweet(stringStatus){
+    return postApiPromise('https://api.twitter.com/1.1/statuses/update.json', {status})
+  }
 }
+
+// TwitterAPI.getSearchResult().then((data)=>{console.log(data);})
+// TwitterAPI.getSearchResult('kadang kadang').then((data)=>{console.log(data);})
+
+// oauth.post('https://api.twitter.com/1.1/statuses/update.json',
+// '920498793220591616-t8vCp2kqKoTkfnibY3gqIFGbLALg7B9', //test user token
+// 'VxhzJe2LfNx87uLX1HCGueuWEiXPzpDuEO7tJV3g67zkC',
+// {status:'shut the fuck up'},
+// function(e, data){
+//   console.log(data);
+// })
